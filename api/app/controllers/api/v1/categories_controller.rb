22c95de9bcd383b9816be8f778d3951e823568d2
alice <a.taiki623@gmail.com>
2024-01-17 16:18:04 +0900

    カテゴリー削除機能の追加

--- api/app/controllers/api/v1/categories_controller.rb
+++ api/app/controllers/api/v1/categories_controller.rb
@@ -60,9 +60,8 @@ class Api::V1::CategoriesController < ApplicationController
       return
     end
 
-    # 新しいカテゴリーを更新
+    # カテゴリーを更新
     category = Category.find(params[:id])
-    Rails.logger.error("CookieからユーザIDの抽出に失敗:::::#{category}")
     category.update(category: update_category_input.category)
     Rails.logger.info("カテゴリーの更新に成功")
 
@@ -106,6 +105,56 @@ class Api::V1::CategoriesController < ApplicationController
     render json: { categories: categories, tasks: tasks }, status: :ok
   rescue => e
     Rails.logger.error("カテゴリーの更新に失敗しました: #{e.message}")
+    render json: { error: e.message }, status: :internal_server_error
+  end
+
+  def destroy
+
+    category = Category.find(params[:id])
+    # カテゴリーと関連タスクを削除
+    category.destroy
+    Rails.logger.info("カテゴリーの削除に成功")
+
+    login_user_id = extract_user_id
+    unless login_user_id
+      Rails.logger.error("CookieからユーザIDの抽出に失敗")
+      return render json: { error: "Failed to extract user ID" }, status: :internal_server_error
+    end
+
+    login_user_group = UserGroup.joins(:users).where(users: { id: login_user_id }).select('user_groups.id').first
+    Rails.logger.info("ログインユーザグループID : #{login_user_group.id}")
+
+    categories = Category.where(user_group_id: login_user_group.id)
+                  .order(:category)
+                  .select('id AS ID', 'category AS Category')
+    Rails.logger.info("カテゴリーの取得に成功")
+
+    tasks = Task.joins(:category)
+            .left_joins(:creator, :responsible)
+            .select(
+                'tasks.id AS ID',
+                'tasks.task AS Task',
+                'tasks.description AS Description',
+                'tasks.start_date AS StartDate',
+                'tasks.status AS Status',
+                'CASE tasks.status WHEN 1 THEN "未着" WHEN 2 THEN "進行中" WHEN 3 THEN "完了" WHEN 4 THEN "Look Back" ELSE "Unknown status" END AS StatusName',
+                'categories.id AS Category',
+                'categories.category AS CategoryName',
+                'tasks.estimate AS Estimate',
+                'responsibles_tasks.id AS Responsible',
+                'responsibles_tasks.name AS ResponsibleUserName',
+                'users.id AS Creator',
+                'users.name AS CreatorUserName',
+                'tasks.created_at AS CreatedAt',
+                'tasks.updated_at AS UpdatedAt'
+            )
+            .where.not(status: 4)
+            .order(created_at: :asc)
+    Rails.logger.info("タスクボード用のタスクの取得に成功")
+
+    render json: { categories: categories, tasks: tasks }, status: :ok
+  rescue => e
+    Rails.logger.error("カテゴリーの削除に失敗しました: #{e.message}")
     render json: { error: e.message }, status: :internal_server_error
   end
 
--- api/app/models/category.rb
+++ api/app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user_group
+  has_many :tasks, dependent: :destroy
 
   validates :category, presence: true, length: { minimum: 1, maximum: 30 }
 end
