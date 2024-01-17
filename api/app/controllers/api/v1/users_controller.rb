2ad94c0ef11d520fd52a6ef6ab119bcbe7ca4d79
alice <a.taiki623@gmail.com>
2024-01-17 09:49:49 +0900

    タスクボードのユーザ一覧取得機能を追加

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,23 @@
 class Api::V1::UsersController < ApplicationController
 
   def index
-    render json: { users: {} }, status: :ok
+    login_user_id = extract_user_id
+    unless login_user_id
+      Rails.logger.error("CookieからユーザIDの抽出に失敗")
+      return render json: { error: "Failed to extract user ID" }, status: :internal_server_error
+    end
+
+    login_user_group_id = UserGroup.joins(:users).where(users: { id: login_user_id }).select('user_groups.id').first
+    Rails.logger.info("ログインユーザグループID : #{login_user_group_id}")
+
+    users = User.where(user_group_id: login_user_group_id).order(:name).select(:id, :name)
+    Rails.logger.info("ユーザのの取得に成功")
+
+    render json: { users: users }, status: :ok
+  rescue => e
+    Rails.logger.error("ユーザの取得に失敗しました: #{e.message}")
+    render json: { error: e.message }, status: :internal_server_error
+  end
   end
 
   def send_email_reset_password
