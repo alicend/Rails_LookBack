10a746e253785d380b055b9598b8b8cb9490547d
alice <a.taiki623@gmail.com>
2024-01-18 10:52:46 +0900

    タスクをルックバックからタスクボードに移動させる機能を追加

--- api/app/controllers/api/v1/tasks_controller.rb
+++ api/app/controllers/api/v1/tasks_controller.rb
@@ -195,6 +195,52 @@ class Api::V1::TasksController < ApplicationController
     render json: { error: e.message }, status: :internal_server_error
   end
 
+  def update_to_completed
+
+    # タスクを更新
+    task = Task.find(params[:id])
+    task.update!(status: 3)
+    Rails.logger.info("タスクの更新に成功")
+
+    login_user_id = extract_user_id
+    unless login_user_id
+      Rails.logger.error("CookieからユーザIDの抽出に失敗")
+      return render json: { error: "Failed to extract user ID" }, status: :internal_server_error
+    end
+
+    login_user_group_id = UserGroup.joins(:users).where(users: { id: login_user_group_id }).select('user_groups.id').first
+    Rails.logger.info("ログインユーザグループID : #{login_user_group_id}")
+
+    tasks = Task.joins(:category)
+            .left_joins(:creator, :responsible)
+            .select(
+                'tasks.id AS ID',
+                'tasks.task AS Task',
+                'tasks.description AS Description',
+                'tasks.start_date AS StartDate',
+                'DATE_FORMAT(tasks.start_date, "%Y-%m-%d") AS StartDate',
+                'tasks.status AS Status',
+                'CASE tasks.status WHEN 1 THEN "未着" WHEN 2 THEN "進行中" WHEN 3 THEN "完了" WHEN 4 THEN "Look Back" ELSE "Unknown status" END AS StatusName',
+                'categories.id AS Category',
+                'categories.category AS CategoryName',
+                'tasks.estimate AS Estimate',
+                'responsibles_tasks.id AS Responsible',
+                'responsibles_tasks.name AS ResponsibleUserName',
+                'users.id AS Creator',
+                'users.name AS CreatorUserName',
+                'DATE_FORMAT(tasks.created_at, "%Y-%m-%d %H:%i") AS CreatedAt',
+                'DATE_FORMAT(tasks.updated_at, "%Y-%m-%d %H:%i") AS UpdatedAt'
+            )
+            .where(status: 4)
+            .order(created_at: :asc)
+    Rails.logger.info("ルックバック用のタスクの取得に成功")
+
+    render json: { tasks: tasks }, status: :ok
+  rescue => e
+    Rails.logger.error("ルックバック用のタスクの取得に失敗しました: #{e.message}")
+    render json: { error: e.message }, status: :internal_server_error
+  end
+
   def destroy
 
     # タスクを削除
