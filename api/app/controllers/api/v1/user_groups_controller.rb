66a73a1cd7b614b14065ce9552bee5e638fa23d9
alice <a.taiki623@gmail.com>
2024-01-19 11:31:26 +0900

    ユーザグループ削除機能を追加

--- api/app/controllers/api/v1/user_groups_controller.rb
+++ api/app/controllers/api/v1/user_groups_controller.rb
@@ -31,4 +31,21 @@ class Api::V1::UserGroupsController < ApplicationController
     Rails.logger.error("ユーザグループ名の更新に失敗しました: #{e.message}")
     render json: { error: e.message }, status: :internal_server_error
   end
+
+  def destroy
+
+    # ユーザグループを削除
+    user_group = UserGroup.find(params[:id])
+    user_group.destroy!
+    Rails.logger.info("ユーザグループの削除に成功")
+
+    cookies.delete(:access_token, httponly: true, secure: true)
+    cookies.delete(:guest_login, httponly: true, secure: true)
+
+    render json: {}, status: :ok
+  rescue => e
+    Rails.logger.error("ユーザグループの削除に失敗しました: #{e.message}")
+    render json: { error: e.message }, status: :internal_server_error
+  end
+
 end
--- api/app/models/user_group.rb
+++ api/app/models/user_group.rb
@@ -1,5 +1,6 @@
 class UserGroup < ApplicationRecord
   has_many :users, dependent: :destroy
+  has_many :categories, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 1, maximum: 30 }
 end
