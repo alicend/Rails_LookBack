9c7960912eae93df2bb6613c10a0ea1e42f79989
alicend <a.taiki623@gmail.com>
2024-02-08 20:36:11 +0900

    リファクタリング

--- api/spec/models/task_spec.rb
+++ api/spec/models/task_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Task, type: :model do
   # バリデーションのテスト
   describe "validations" do
-    it "is valid with valid attributes" do
+    it "全ての項目が有効" do
       user_group = UserGroup.create!(name: "Example Group")
       creator = User.create!(name: "Creator User", email: "creator@example.com", password: "password", user_group:)
       responsible = User.create!(name: "Responsible User", email: "responsible@example.com", password: "password", user_group:)
@@ -13,7 +13,7 @@ RSpec.describe Task, type: :model do
       expect(task).to be_valid
     end
 
-    it "is invalid without a task name" do
+    it "タスク名がない場合は無効" do
       user_group = UserGroup.create!(name: "Example Group")
       creator = User.create!(name: "Creator User", email: "creator@example.com", password: "password", user_group:)
       responsible = User.create!(name: "Responsible User", email: "responsible@example.com", password: "password", user_group:)
@@ -22,7 +22,26 @@ RSpec.describe Task, type: :model do
       expect(task).not_to be_valid
     end
 
-    it "is invalid without a description" do
+    it "タスク名が0文字は無効" do
+      user_group = UserGroup.create!(name: "Example Group")
+      creator = User.create!(name: "Creator User", email: "creator@example.com", password: "password", user_group:)
+      responsible = User.create!(name: "Responsible User", email: "responsible@example.com", password: "password", user_group:)
+      category = Category.create!(category: "Example Category", user_group:)
+      task = Task.new(task: "", description: "Example Description", status: 1, estimate: 5, creator:, responsible:, category:)
+      expect(task).not_to be_valid
+    end
+
+    it "タスク名が31文字以上の場合は無効" do
+      user_group = UserGroup.create!(name: "Example Group")
+      creator = User.create!(name: "Creator User", email: "creator@example.com", password: "password", user_group:)
+      responsible = User.create!(name: "Responsible User", email: "responsible@example.com", password: "password", user_group:)
+      category = Category.create!(category: "Example Category", user_group:)
+      task_name = "a" * 31
+      task = Task.new(task: task_name, description: "Example Description", status: 1, estimate: 5, creator:, responsible:, category:)
+      expect(task).not_to be_valid
+    end
+
+    it "説明がない場合は無効" do
       user_group = UserGroup.create!(name: "Example Group")
       creator = User.create!(name: "Creator User", email: "creator@example.com", password: "password", user_group:)
       responsible = User.create!(name: "Responsible User", email: "responsible@example.com", password: "password", user_group:)
@@ -31,7 +50,26 @@ RSpec.describe Task, type: :model do
       expect(task).not_to be_valid
     end
 
-    it "is invalid without a status" do
+    it "説明が0文字は無効" do
+      user_group = UserGroup.create!(name: "Example Group")
+      creator = User.create!(name: "Creator User", email: "creator@example.com", password: "password", user_group:)
+      responsible = User.create!(name: "Responsible User", email: "responsible@example.com", password: "password", user_group:)
+      category = Category.create!(category: "Example Category", user_group:)
+      task = Task.new(task: "Example Task", description: "", status: 1, estimate: 5, creator:, responsible:, category:)
+      expect(task).not_to be_valid
+    end
+
+    it "説明が256文字以上の場合は無効" do
+      user_group = UserGroup.create!(name: "Example Group")
+      creator = User.create!(name: "Creator User", email: "creator@example.com", password: "password", user_group:)
+      responsible = User.create!(name: "Responsible User", email: "responsible@example.com", password: "password", user_group:)
+      category = Category.create!(category: "Example Category", user_group:)
+      description = "a" * 256
+      task = Task.new(task: "Example Task", description:, status: 1, estimate: 5, creator:, responsible:, category:)
+      expect(task).not_to be_valid
+    end
+
+    it "ステータスがない場合は無効" do
       user_group = UserGroup.create!(name: "Example Group")
       creator = User.create!(name: "Creator User", email: "creator@example.com", password: "password", user_group:)
       responsible = User.create!(name: "Responsible User", email: "responsible@example.com", password: "password", user_group:)
@@ -41,7 +79,7 @@ RSpec.describe Task, type: :model do
       expect(task).not_to be_valid
     end
 
-    it "is invalid with a status out of range" do
+    it "ステータスが範囲外の場合は無効" do
       user_group = UserGroup.create!(name: "Example Group")
       creator = User.create!(name: "Creator User", email: "creator@example.com", password: "password", user_group:)
       responsible = User.create!(name: "Responsible User", email: "responsible@example.com", password: "password", user_group:)
@@ -51,17 +89,7 @@ RSpec.describe Task, type: :model do
       expect(task).not_to be_valid
     end
 
-    it "is valid with an estimate within range" do
-      user_group = UserGroup.create!(name: "Example Group")
-      creator = User.create!(name: "Creator User", email: "creator@example.com", password: "password", user_group:)
-      responsible = User.create!(name: "Responsible User", email: "responsible@example.com", password: "password", user_group:)
-      category = Category.create!(category: "Example Category", user_group:)
-      task = Task.new(task: "Example Task", description: "Example Description", status: 1, estimate: 100, creator:, responsible:,
-                      category:)
-      expect(task).to be_valid
-    end
-
-    it "is invalid with an estimate out of range" do
+    it "見積もりが範囲外の場合は無効" do
       user_group = UserGroup.create!(name: "Example Group")
       creator = User.create!(name: "Creator User", email: "creator@example.com", password: "password", user_group:)
       responsible = User.create!(name: "Responsible User", email: "responsible@example.com", password: "password", user_group:)
@@ -73,7 +101,7 @@ RSpec.describe Task, type: :model do
   end
 
   # アソシエーションのテスト
-  describe "associations" do
+  describe "アソシエーション" do
     it { should belong_to(:creator).class_name("User").inverse_of(:created_tasks) }
     it { should belong_to(:responsible).class_name("User").inverse_of(:responsible_tasks) }
     it { should belong_to(:category) }
