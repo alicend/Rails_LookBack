ed6c2a47a5ce7d1a2d89b94f4277c61890da5165
alicend <a.taiki623@gmail.com>
2024-02-08 20:19:30 +0900

    リファクタリング

--- api/spec/models/user_group_spec.rb
+++ api/spec/models/user_group_spec.rb
@@ -2,31 +2,36 @@ require "rails_helper"
 
 RSpec.describe UserGroup, type: :model do
   # バリデーションのテスト
-  describe "validations" do
-    it "is valid with a valid name" do
+  describe "バリデーション" do
+    it "全ての項目が有効" do
       user_group = UserGroup.new(name: "Example Group")
       expect(user_group).to be_valid
     end
 
-    it "is invalid without a name" do
+    it "名前がない場合は無効" do
       user_group = UserGroup.new(name: nil)
       expect(user_group).not_to be_valid
     end
 
-    it "is invalid with a name too short" do
+    it "名前が０文字の場合は無効" do
       user_group = UserGroup.new(name: "")
       expect(user_group).not_to be_valid
     end
 
-    it "is invalid with a name too long" do
+    it "名前が31文字以上の場合は無効" do
       user_group = UserGroup.new(name: "a" * 31)
       expect(user_group).not_to be_valid
     end
   end
 
   # アソシエーションのテスト
-  describe "associations" do
-    it { should have_many(:users).dependent(:destroy) }
-    it { should have_many(:categories).dependent(:destroy) }
+  describe "アソシエーション" do
+    it "多数のユーザーを持ち、ユーザーグループが削除されるとユーザーも削除される" do
+      should have_many(:users).dependent(:destroy)
+    end
+
+    it "多数のカテゴリーを持ち、ユーザーグループが削除されるとカテゴリーも削除される" do
+      should have_many(:categories).dependent(:destroy)
+    end
   end
 end
