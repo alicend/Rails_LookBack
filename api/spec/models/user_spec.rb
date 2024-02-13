5b4553040ebf52bdcdea2433a74e115a47f0eb43
alicend <a.taiki623@gmail.com>
2024-02-13 21:47:54 +0900

    リファクタリング

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   describe "ヴァリデーション" do
     it "全ての項目が有効" do
       user_group = UserGroup.create!(name: "Example Group")
-      user = User.new(name: "John Doe", email: "john@example.com", password: "password", password_confirmation: "password", user_group:)
+      user = User.new(name: "TestUser", email: "john@example.com", password: "password", password_confirmation: "password", user_group:)
       expect(user).to be_valid
     end
 
@@ -16,13 +16,13 @@ RSpec.describe User, type: :model do
 
     it "メールがない場合は無効" do
       user_group = UserGroup.create!(name: "Example Group")
-      user = User.new(name: "John Doe", email: nil, password: "password", password_confirmation: "password", user_group:)
+      user = User.new(name: "TestUser", email: nil, password: "password", password_confirmation: "password", user_group:)
       expect(user).not_to be_valid
     end
 
     it "メールが重複している場合は無効" do
       user_group = UserGroup.create!(name: "Example Group")
-      User.create!(name: "John Doe", email: "john@example.com", password: "password", password_confirmation: "password", user_group:)
+      User.create!(name: "TestUser", email: "john@example.com", password: "password", password_confirmation: "password", user_group:)
       user = User.new(name: "Jane Doe", email: "john@example.com", password: "password", password_confirmation: "password", user_group:)
       expect(user).not_to be_valid
     end
@@ -37,19 +37,19 @@ RSpec.describe User, type: :model do
     it "メールが256文字以上の場合は無効" do
       user_group = UserGroup.create!(name: "Example Group")
       email = "#{"a" * 244}@example.com"
-      user = User.new(name: "John Doe", email:, password: "password", password_confirmation: "password", user_group:)
+      user = User.new(name: "TestUser", email:, password: "password", password_confirmation: "password", user_group:)
       expect(user).not_to be_valid
     end
 
     it "パスワードがない場合は無効" do
       user_group = UserGroup.create!(name: "Example Group")
-      user = User.new(name: "John Doe", email: "john@example.com", password: nil, password_confirmation: "password", user_group:)
+      user = User.new(name: "TestUser", email: "john@example.com", password: nil, password_confirmation: "password", user_group:)
       expect(user).not_to be_valid
     end
 
     it "パスワードとパスワード確認が一致しない場合は無効" do
       user_group = UserGroup.create!(name: "Example Group")
-      user = User.new(name: "John Doe", email: "john@example.com", password: "password", password_confirmation: "different", user_group:)
+      user = User.new(name: "TestUser", email: "john@example.com", password: "password", password_confirmation: "different", user_group:)
       expect(user).not_to be_valid
     end
   end
